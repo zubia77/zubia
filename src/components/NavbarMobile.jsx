3e1cfe60af722c79462b6ec51bb16b028c57e6f8
Zubia <rashzubia@gmail.com>
2022-10-30 21:29:21 +0100

    navbar correction mobile version

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -9,7 +9,7 @@ function NavbarMobile() {
 
     return (
         <div className="md:hidden">
-            <div className="w-full relative text-5xl flex overflow-auto items-center justify-center rounded-full w-20 h-20 m-5 bg-palette-80 ring-0 ring-palette-70 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md md:hidden">
+            <div className="w-full relative text-5xl flex overflow-auto items-center justify-center rounded-full w-20 h-20 m-5 bg-color-10 ring-0 ring-color-50 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md md:hidden">
                 {showNav ? (
                     <BiX onClick={() => setShowNav(!showNav)} className="" />
                 ) : (
@@ -18,12 +18,12 @@ function NavbarMobile() {
                 <div
                     className={
                         (showNav ? "left-0" : "-left-full") +
-                        " shadowGlasmorphic bg-color-30/25  shadow-outer w-72 fixed left-0 top-24 bottom-20 z-40 text-palette-50 flex justify-center text-center text-2xl rounded-br-3xl rounded-tr-3xl transition-ease-in duration-1000 backdrop-blur-md"
+                        " shadowGlasmorphic bg-color-30/25  shadow-outer w-72 fixed left-0 top-24  z-40 text-palette-50 flex justify-center text-center text-2xl rounded-br-3xl rounded-tr-3xl transition-ease-in duration-1000 backdrop-blur-md"
                     }
                     onClick={() => setShowNav(!showNav)}
                 >
                     {" "}
-                    <nav className="gradient flex flex-col items-center justify-center m-10 gap-40">
+                    <nav className="gradient flex flex-col items-center justify-center m-10 gap-20">
                         <NavLink to="/home" className="px-8">
                             Home
                         </NavLink>
