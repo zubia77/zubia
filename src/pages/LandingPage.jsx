6bf7dd4db32b4312d2d7fbb017ece175e76f4bdd
Zubia <rashzubia@gmail.com>
2022-11-01 14:42:46 +0100

    Landingpage logo position correction

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,11 +3,11 @@ import Navbar from "../components/Navbar";
 import NavbarMobile from "../components/NavbarMobile";
 function Landingpage() {
     return (
-        <div className="h-screen xl:h-[120vh] w-screen flex flex-col bg-color-10 text-color-20">
+        <div className="h-screen lg:h-[110vh]  w-screen flex flex-col bg-color-10 text-color-20">
             <Navbar />
             <NavbarMobile />
        <div className="w-screen xl:flex xl:flex-row-reverse">
-                <div className="h-full bg-main bg-contain xl:bg-cover bg-no-repeat -mt-20 md:flex md:bg-right-top lg:mt-0 xl:w-[50vw]"></div>
+                <div className="h-full bg-main bg-contain xl:bg-cover bg-no-repeat -mt-28 ml-32 md:flex md:bg-right-top lg:mt-0 xl:w-[50vw]"></div>
                 <div className="h-[50vh] xl:w-[50vw] xl:h-[100vh] font-style1 flex flex-col items-center justify-start xl:justify-center">
                     <h2 className="greeting text-center text-4xl xl:text-7xl pb-20">
                         Hi I'm
@@ -15,7 +15,7 @@ function Landingpage() {
                     <h1 className="name text-center pb-20 text-4xl md:text-7xl xl:text-9xl">
                         Zubi
                     </h1>
-                    <div className="w-max lg:w-[65%] xl:w-[30%]">
+                    <div className="w-max lg:w-[65%] xl:w-[42%]">
                         <h3 className="profession text-center text-3xl font-style2">
                             A Frontend Developer{" "}
                         </h3>
