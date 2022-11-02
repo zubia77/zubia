36356152309bfd0e7c21d030ffe0c030df201812
Zubia <rashzubia@gmail.com>
2022-11-02 20:41:06 +0100

    contact height

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -35,14 +35,14 @@ function Contact() {
 
     return (
         <div className="w-screen">
-            <div className="h-screen text-color-20 flex flex-col  bg-color-10">
+            <div className=" text-color-20 flex flex-col  bg-color-10">
                 <Navbar />
                 <NavbarMobile />
-                <div className="w-[100vw] mt-20 flex flex-col justify-center items-center shrink">
-                    <h1 className="px-10 text-xl text-color-20 font-style10 md:text-5xl">
+                <div className="w-[100vw]  md:mt-10 flex flex-col justify-center items-center shrink">
+                    <h1 className="px-10 text-3xl text-color-20 font-style10 md:text-5xl">
                         Please get in touch!
                     </h1>
-                    <div className="m-6">
+                    <div className="m-6 h-[100vh]">
                 <form
                     onSubmit={sendEmail}
                     className="w-[80vw] lg:w-[60vw] xl:w-[45vw] bg-gradient-to-b from-color-50 to-color-30 border-8 border-color-40 py-8 flex justify-center rounded-2xl shadow-outer"
@@ -70,7 +70,7 @@ function Contact() {
                             <textarea
                                 type="text"
                                 required
-                                className="w-72 md:w-96  h-60 p-4 rounded-2xl bg-color-30 border-4 border-color-10 text-color-50 placeholder:text-color-20  outline-none"
+                                className="w-72 md:w-96  h-40 p-4 rounded-2xl bg-color-30 border-4 border-color-10 text-color-50 placeholder:text-color-20  outline-none"
                                 name="message"
                                 id=""
                                 cols={3}
